cb0af6323da9be5fe51d31cdaa95e9b49605806c
slampalo <slam@palo-it.com>
2022-11-16 14:01:29 +0800

    minor update

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express/interfaces/nest-express-application.interface';
 import { AppModule } from './app.module';
+import env from './config/env';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({origin:"*"});
-  await app.listen(3000);
+  await app.listen(env().port);
 }
 bootstrap();
