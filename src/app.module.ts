69b595d22dd6b4be156236ea76caa64cb65ea2c0
alphalam1331 <110608781+alphalam1331@users.noreply.github.com>
2023-05-04 00:00:55 +0800

    Add app controller (#14)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { dataSource } from './config/db';
 import { PostsModule } from './posts/posts.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [TypeOrmModule.forRoot(dataSource), UsersModule, PostsModule],
+  controllers: [AppController],
 })
 export class AppModule {}
